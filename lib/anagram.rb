a12b23db4d0d555c72b6155c7d7d9ee71e613163
Erick Ochieng <erickochieng766@gmail.com>
2023-02-08 13:11:49 +0300

    Passed Test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.filter { |wd| wd.split("").sort == word.split("").sort }
+  end
+end
+
+listen = Anagram.new("listen")
+puts listen.word
+puts listen.match(%w{enlists google inlets banana})
